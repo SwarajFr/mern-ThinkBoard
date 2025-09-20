edde37b224e1f31bdb106291a21ba84b510c349f
Swaraj <swarajpanda331@gmail.com>
2025-09-20 15:05:21 +0530

    Updated Create Page Card UI

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -48,6 +48,7 @@ export const CreatePage = () => {
           <ArrowLeftIcon className="size-5" />
           Back To Notes
           </Link>
+          <div className="card">
             <div className="card-body">
               <h2 className="card-title text-2xl mb-4">Create New Note</h2>
               <form onSubmit={handleSubmit}>
@@ -86,6 +87,7 @@ export const CreatePage = () => {
           </div>
         </div>
       </div>
+    </div>
   );
 }
 
